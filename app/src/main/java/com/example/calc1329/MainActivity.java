847f76f3879c021c04bf7b9acb0871f13f2c8a70
dkuivashev <dkuvashev@fmschool72.ru>
2019-11-14 16:30:32 +0300

    Добавил случайное число!!!!

--- app/src/main/java/com/example/calc1329/MainActivity.java
+++ app/src/main/java/com/example/calc1329/MainActivity.java
@@ -4,18 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 100;
-        int b = 200;
-        int c = a+b;
-        int g = a/b;
-        int l = g + c;
-        System.out.println(l);
-        //fghfghgfhfghfghfghfgh
+        Random r = new Random();
+        int x = r.nextInt(2000);
     }
 }
