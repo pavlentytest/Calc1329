087eacedad387bd394591b6ef4c0d2d85786eb0f
Michael <UnderHamster@mail.ru>
2019-11-14 16:36:46 +0300

    делал вывод

--- app/src/main/java/com/example/calc1329/MainActivity.java
+++ app/src/main/java/com/example/calc1329/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.calc1329;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,9 @@ public class MainActivity extends AppCompatActivity {
         int a = 100;
         int b = 200;
         int c = a+b;
-        int g = a/b;
-        int l = g + c;
-        System.out.println(l);
-        //fghfghgfhfghfghfghfgh
+
+        TextView tv = findViewById(R.id.textView);
+        tv.setText(""+c);
+
     }
 }
